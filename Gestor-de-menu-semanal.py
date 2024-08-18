b6949d4dc2ca88bd5b600c25cb322c315b8e8d2e
Gerar22021 <Gerar2_yo@hotmail.com>
2024-08-18 17:10:06 -0300

    empezando

--- Gestor-de-menu-semanal.py
+++ Gestor-de-menu-semanal.py
@@ -1,3 +1,9 @@
+# Realizar un programa que pueda agregar platos a un menu y organizarlos
+# en los dias de la semana de forma aleatoria pero teniendo en cuenta la disponibilidad
+# de ingredientes (los cuales tambien se puedan agregar a un stock de almacen)
+# 
+
+
 # menu
 # stock
 # ingredientes
