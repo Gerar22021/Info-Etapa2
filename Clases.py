87362ff1bbdd50cd8320e0175f983a8dd9f9e575
Gerar22021 <Gerar2_yo@hotmail.com>
2024-08-18 12:42:41 -0300

    empezando

---
+++ Clases.py
@@ -0,0 +1,9 @@
+def Menu():
+    def __init__(self, nombre, ingredientes, receta):
+        self.nombre = nombre
+        self.ingredientes = [ingredientes]
+        self.receta = receta
+
+def Stock():
+    pass
+
