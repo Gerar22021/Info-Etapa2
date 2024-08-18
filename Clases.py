087747f1b04405b3f448c5a94e21c83220205874
Gerar22021 <Gerar2_yo@hotmail.com>
2024-08-18 17:27:51 -0300

    empezando

--- Clases.py
+++ Clases.py
@@ -1,9 +1,15 @@
+def Plato():
+    pass
+
+def Ingrediente():
+    pass
+
 def Menu():
     def __init__(self, nombre, ingredientes, receta):
         self.nombre = nombre
         self.ingredientes = [ingredientes]
         self.receta = receta
 
-def Stock():
+def Almacen():
     pass
 
--- Gestor-de-menu-semanal.py
+++ Gestor-de-menu-semanal.py
@@ -1,10 +1,14 @@
 # Realizar un programa que pueda agregar platos a un menu y organizarlos
 # en los dias de la semana de forma aleatoria pero teniendo en cuenta la disponibilidad
 # de ingredientes (los cuales tambien se puedan agregar a un stock de almacen)
-# 
+# La idea es que nos mantega al tanto de lo que falte en el almacen para poder realizar los platos del menu
+# Tambien se puede tener dos opciones, organizar la semana de forma aleatoria o personalizada
 
+#### Clases ####
 
-# menu
-# stock
-# ingredientes
-# 
+#---- Plato
+#---- ingrediente
+#---- menu
+#---- almacen
+
+from Clases import Plato,Ingrediente,Menu,Almacen
