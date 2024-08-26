6845e4ddac6b0a9bb4fe7205795f0d48fbd3bc6c
Gerar22021 <Gerar2_yo@hotmail.com>
2024-08-25 23:35:32 -0300

    Se esta trabajando en la funcion organizar semana

--- Gestor-de-menu-semanal.py
+++ Gestor-de-menu-semanal.py
@@ -20,18 +20,6 @@ from Clases import Plato,Ingrediente,Menu,Almacen
 plato0 = Plato('---------PLATOS---------')
 plato1 = Plato('Guiso de arroz con pollo', '1)- Dorar el pollo en un poco de aceite. Retirar y reservar.\n2)- Dorar la cebolla picada en la misma olla donde doramos el pollo.\n3)- Agregar el tomate triturado, el puré, los condimentos a gusto, el azúcar y 1/2 litro de agua. Volver a introducir el pollo y dejar cocinar 20 minutos.\n4)- Luego agregar el arroz, el caldito, revolver y dejar cocinar hasta que el arroz este cocido al punto deseado. Mientras se va cociendo, hay que ir agregando agua y revolviendo de tanto en tanto para controlar y evitar que la preparación se seque.\n5)- Finalmente incorporar las arvejas para que se calienten, probar, alinear de sal y listo!...A comer!', '1 pollo trozado en 8\n1 cebolla grande\n1 morrón\n1 tomate')
 plato2 = Plato('Tarta de atun', '1)-Picar la cebolla, el morrón y el ajo y rehogarlos en aceite.\n2)-Cuando la cebolla esté transparente, agregar el tomate fresco picado y cocer hasta que tome un color rojo oscuro.\n3)-Agregar el atún y remover hasta integrar. Incorporar también la cucharada de puré de tomates.\n4)-Incorporar los huevos batidos y salpimentar. Agregar el orégano (muy poco).\n5)-Si te gustan, agregar las aceitunas picadas.\n6)-Colocar todo en una tartera chica.\n7)-Cerrar los bordes y llover con semillas de sésamo arriba.\n8)-Llevar a horno fuerte hasta que el hojaldre se vea dorado. ¡Listo! Ya tenés tu tarta de atún.', '1 masa de hojaldre\n1/2 cebolla\n1/2 morrón rojo\n1 diente de ajo\n1 tomate chico\n1 cda. de puré de tomates\n2 latas de atún\n1 papa chica\n1 pizca de orégano\n2 huevos\nAceitunas (opcional)\nSemillas de sésamo\nSal, pimienta y aceite.')
-plato3 = Plato()
-plato4 = Plato()
-plato5 = Plato()
-plato6 = Plato()
-plato7 = Plato()
-plato8 = Plato()
-plato9 = Plato()
-plato10 = Plato()
-plato11 = Plato()
-plato12 = Plato()
-plato13 = Plato()
-plato14 = Plato()
 
 listaDePlatos = []
 
@@ -45,21 +33,48 @@ menu1 = Menu('Semana 1')
 menu1.platos.append(plato0)
 menu1.platos.append(plato1)
 menu1.platos.append(plato2)
-menu1.platos.append(plato3)
-menu1.platos.append(plato4)
-menu1.platos.append(plato5)
-menu1.platos.append(plato6)
-menu1.platos.append(plato7)
-menu1.platos.append(plato8)
 
 listaMenus.append(menu1)
 
 # IMPLEMENTANDO LA INTERFAZ GRÁFICA
 
 import tkinter as tk
+import time
+
 ventana = tk.Tk()
 ventana.title('Gestor de menú semanal')
 ventana.geometry('400x200')
+
+etiqueta1 = tk.Label(ventana, text='LUNES:', justify= 'left')
+etiqueta2 = tk.Label(ventana, text='MARTES:')
+etiqueta3 = tk.Label(ventana, text='MIERCOLES:')
+etiqueta4 = tk.Label(ventana, text='JUEVES:')
+etiqueta5 = tk.Label(ventana, text='VIERNES:')
+etiqueta6 = tk.Label(ventana, text='SABADO:')
+etiqueta7 = tk.Label(ventana, text='DOMINGO:')
+
+etiqueta1.grid(row=1, column=0)
+etiqueta2.grid(row=2, column=0)
+etiqueta3.grid(row=3, column=0)
+etiqueta4.grid(row=4, column=0)
+etiqueta5.grid(row=5, column=0)
+etiqueta6.grid(row=6, column=0)
+etiqueta7.grid(row=7, column=0)
+
+def organizar():
+    pass
+
+boton_organizar = tk.Button(ventana, text='Organizar de forma aleatoria', command=organizar)
+boton_organizar.grid(row=0,column=0)
+
+# reloj = tk.Label(ventana, font =
+# ('Arial', 60), bg = 'blue', fg = 'white')
+# def hora():
+#     tiempo_actual =time.strftime('%H:%M')
+#     reloj.config(text = tiempo_actual, height=50, width=50)
+# ventana.after(1000, hora)
+# reloj.pack(anchor = 'center')
+# hora()
 
 def agregarPlato():
     ventana = tk.Toplevel()
@@ -117,23 +132,37 @@ def agregarPlato():
             lista_platos.insert(tk.END, f'{plato.nombre}')
     lista_platos.grid(row=6, column=1)
 
+    # cajaNombre = tk.Text(ventana, width=10, height=10)
+    # cajaIngredientes = tk.Text(ventana, width=10, height=10)
+    # cajaReceta = tk.Text(ventana, width=10, height=10)
+    # cajaNombre.grid(row=6,column=2)
+    # cajaIngredientes.grid(row=6,column=3)
+    # cajaReceta.grid(row=6,column=4)
+    # etiquetaNombre = tk.Label(ventana)
+    # etiquetaNombre.grid(row=6,column=2, ipadx=0, ipady=0)
+    # etiquetaIngredientes = tk.Label(ventana)
+    # etiquetaIngredientes.grid(row=6,column=3, ipadx=0, ipady=0)
+    # etiquetaReceta = tk.Label(ventana)
+    # etiquetaReceta.grid(row=6,column=4, ipadx=0, ipady=0)
+
+
     def mostrar():
         seleccion = lista_platos.curselection()
         if seleccion:
              for c in seleccion:
                 if int(c):
                     etiquetaNombre = tk.Label(ventana, text= listaDePlatos[c].nombre)
+                    etiquetaNombre.config(text=listaDePlatos[c].nombre)
                     etiquetaNombre.grid(row=6,column=2, ipadx=0, ipady=0)
                     etiquetaIngredientes = tk.Label(ventana, text= listaDePlatos[c].ingredientes)
+                    etiquetaIngredientes.config(text=listaDePlatos[c].ingredientes)
                     etiquetaIngredientes.grid(row=6,column=3, ipadx=0, ipady=0)
                     etiquetaReceta = tk.Label(ventana, text= listaDePlatos[c].receta)
+                    etiquetaReceta.config(text=listaDePlatos[c].receta)
                     etiquetaReceta.grid(row=6,column=4, ipadx=0, ipady=0)
-                    print(listaDePlatos[c].nombre)
 
     boton_mostrar = tk.Button(ventana, text = 'Mostrar', command = mostrar)
     boton_mostrar.grid(row=7, column=1)
-
-    
 
 def agregarMenu():
     ventana = tk.Toplevel()
@@ -158,7 +187,6 @@ def agregarMenu():
     lista_tareas = tk.Listbox(ventana)
     lista_tareas.pack()
     
-
 def modificarPlato():
     ventana = tk.Toplevel()
     ventana.title('Modificar plato' )
@@ -218,12 +246,12 @@ submenu2.add_command(label = 'modificar', command= modificarMenu)
 
 
 
-caja1 = tk.Listbox(ventana)
-caja2 = tk.Listbox(ventana)
-caja3 = tk.Listbox(ventana)
-caja4 = tk.Listbox(ventana)
-caja5 = tk.Listbox(ventana)
-caja6 = tk.Listbox(ventana)
+# caja1 = tk.Listbox(ventana)
+# caja2 = tk.Listbox(ventana)
+# caja3 = tk.Listbox(ventana)
+# caja4 = tk.Listbox(ventana)
+# caja5 = tk.Listbox(ventana)
+# caja6 = tk.Listbox(ventana)
 # caja1.grid(row=1,column=1)
 # caja2.grid(row=1,column=2)
 # caja3.grid(row=1,column=3)
